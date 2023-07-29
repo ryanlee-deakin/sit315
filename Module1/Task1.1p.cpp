baa15f9c43f64b47d74f55d4a0a4571ca5a033a8
ryanlee-deakin <127084247+ryanlee-deakin@users.noreply.github.com>
2023-07-29 11:34:43 +0800

    Replace button with ultrasonic sensor

--- Module1/Task1.1p.cpp
+++ Module1/Task1.1p.cpp
@@ -1,13 +1,27 @@
-const int button = 2;
+const int sensor = 2;
+const long sensorLimit = 19000;
 const int led = 13;
 int counter;
 int buttonState = 0;
 
+// function readUltrasonicDistance is example code
+// from public domain (http://www.arduino.cc/en/Tutorial/Ping)
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
 void setup() {
   Serial.begin(9600);
-  pinMode(button, INPUT);
-  Serial.print("INIT: Pin used for INPUT: ");
-  Serial.println(button);
   pinMode(led, OUTPUT);
   Serial.print("INIT: Pin used for OUTPUT: ");
   Serial.println(led);
@@ -16,8 +30,12 @@ void setup() {
 
 void loop()
 {
-  if (digitalRead(button) == HIGH) {
-      Serial.println("Button state is HIGH");
+  // sense echo travel time in microseconds
+  long echoReading = readUltrasonicDistance(sensor,sensor);
+  // trigger LED if sensor detects echo returning under sensorLimit value
+  if (echoReading < sensorLimit) {
+    Serial.print("Echo reading is: ");
+    Serial.println(echoReading);
     for (counter = 0; counter < 3; ++counter) {
       digitalWrite(led, HIGH);
       delay(250);
@@ -25,9 +43,6 @@ void loop()
       Serial.println(counter);
       digitalWrite(led, LOW);
       delay(250);
-    }
-    if (digitalRead(button) == LOW) {
-      Serial.println("Button state is LOW");
     }
   }
 }
